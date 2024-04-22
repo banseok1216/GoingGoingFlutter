cbb714e889f97cfd4296da8f80b77e0009127803
banseok1216 <bansuk1216@naver.com>
2024-04-22 20:53:20 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-gimport 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:goinggoing_flutterapp/Calender/CalenderDetailsScreen.dart';
